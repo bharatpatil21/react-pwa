a006b983d1f172eb8b7083253c8ad213dfb8fbf0
bharat_patil <patilbharat09@gmail.com>
2020-08-25 10:10:14 +0530

    Added routing with three component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Nav, Navbar } from "react-bootstrap";
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
+import Users from "./Users";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="primary" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>
-      </Navbar>
+      <Router>
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/users">Users</Nav.Link>
+            {/* <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+            <Nav.Link >
+              <Link to="/users">Users</Link>
+            </Nav.Link> */}
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route component={About} path="/about"></Route>
+          <Route component={Users} path="/users"></Route>
+          <Route component={Home} path="/"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
