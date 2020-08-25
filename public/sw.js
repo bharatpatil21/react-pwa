a49a043a811c9c7d105f094a57623b18b8e6c578
bharat_patil <patilbharat09@gmail.com>
2020-08-25 10:39:55 +0530

    Added static data in cache and read data also

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,23 @@
-console.log('SW in public folder')
+const cacheData = "appV1";
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        "/static/js/bundle.js",
+        "/static/js/0.chunk.js",
+        "/static/js/main.chunk.js",
+        "/index.html",
+        "/",
+      ]);
+    })
+  );
+});
+
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((result) => {
+      if (result) return result;
+    })
+  );
+});
